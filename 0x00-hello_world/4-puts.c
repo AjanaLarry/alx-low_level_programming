63ffc55966b2d6fe786d8a82d96040305ff739d2
Juwon Ajana <juwon4real007@gmail.com>
2023-03-16 17:59:33 -0700

    Task 4 - update code descripion for Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/*
+ *function main - entry point of the code
+ *
+ *puts - prints exactly "Programming is like building a multilingual puzzle, followed by a new line."
+ *
+ *return - ends the function main()
+ */
+
+
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
