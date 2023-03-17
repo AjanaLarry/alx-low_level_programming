9462104a25aac322007196f9dfef3c40c6ed5c26
Juwon Ajana <juwon4real007@gmail.com>
2023-03-16 18:14:08 -0700

    Task 5 - Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of the code
+ *
+ *Description: prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ *Return: ends the function main()
+ */
+
+
+
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
