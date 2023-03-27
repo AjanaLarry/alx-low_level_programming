116beb3c4be522b0604fec93ebe6de7c3c5d1888
Juwon Ajana <juwon4real007@gmail.com>
2023-03-27 14:07:43 -0700

    Task 0 - 98 Battery st.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -25,4 +25,14 @@ void print_most_numbers(void);
 int mul(int a, int b);
 void print_number(int n);
 int b10_pow(int idx);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif
