58e81860a9113179347e92007bcb1c2ced2c3bf2
Juwon Ajana <juwon4real007@gmail.com>
2023-03-30 18:46:48 -0700

    Task 0 - strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,22 @@
 
 /**
  * _strcat - function that concatenates two strings
- * @dest: takes a string
- * @src: takes a string
- * Return: a pointer to the resulting string dest
+ * @dest: destination string
+ * @src: source string
+ * Return: a pointer to the resulting string
  */
 
 char *_strcat(char *dest, char *src)
 {
+	int l;
+	int i;
 
-
+	for (l = 0; dest[l] != '\0'; l++)
+	{
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[l + i] = src[i];
+	}
+	return (dest);
 }
