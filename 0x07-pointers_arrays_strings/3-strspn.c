f80ca8e4719766bf1b1ad741fb307da2515e3b7d
Juwon Ajana <juwon4real007@gmail.com>
2023-04-03 18:13:46 -0700

    Task 3 - strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,20 +10,20 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j;
-	unsigned int count = 0;
-	
-	for  (i = 0; s[i] != '\0'; i++)
+	int l = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				count++;
+				l++;
 				break;
 			}
 		}
 		if (accept[j] == '\0')
-			return (count);
+			return (l);
 	}
-	return (count);
+	return (l);
 }
