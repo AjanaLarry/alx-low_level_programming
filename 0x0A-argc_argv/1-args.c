4f859163ac274f2ff65a91c6aeb84291b216a7bd
Juwon Ajana <juwon4real007@gmail.com>
2023-04-12 03:55:52 -0700

    Update - The best argument against democracy

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,9 +11,7 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int args = argc - 1;
-	
 	if (argc > 0)
-		printf("%d\n", args);
+		printf("%d\n", argc - 1);
 	return (0);
 }
