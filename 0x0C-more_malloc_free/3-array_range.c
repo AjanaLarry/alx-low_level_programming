05c0ecb7bee8959c90368b621494477cd628d43a
Juwon Ajana <juwon4real007@gmail.com>
2023-04-13 19:58:37 -0700

    Task 3 - update array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,26 +4,25 @@
 
 /**
 * array_range - a function that creates an array of integers
-* @min: minimum int
-* @max: maximum int
-* Return: pointer to allocated memory
+* @min: minimum values
+* @max: maximum values
+* Return: returns the pointer to the newly created array
 **/
 
 int *array_range(int min, int max)
 {
-	int *arr;
-       	int diff, i;
+	int *p;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
-	diff = max - min + 1;
-	arr = malloc(sizeof(int) * size);
-	if (arr == NULL)
+	size = max - min + 1;
+	p = malloc(sizeof(int) * size);
+	if (p == NULL)
 		return (NULL);
-
-	for (i = 0; i < diff; i++)
+	for (i = 0; i < size; i += 1)
 	{
-		arr[i] = min + i;
+		p[i] = min + i;
 	}
-	return (arr);
+	return (p);
 }
