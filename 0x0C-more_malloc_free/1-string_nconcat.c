efb094b0ddfda6feb569d9c20bf6d1c6e1d282d8
Juwon Ajana <juwon4real007@gmail.com>
2023-04-13 19:45:36 -0700

    Task 1 - update string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,45 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
-* string_nconcat - a function that concatenates two strings
-* @s1: string 1
-* @s2: string 2
+* string_nconcat - concatentates two strings using the newly allocated memory
+* @s1: first string
+* @s2: second string
 * @n: unsigned int
-* Return: pointer to new string or NULL if it fails
+* Return: a pointer to 2 string concat..
 **/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        int len1, i;
-        unsigned int j;
-        char *arr;
+	int l, i;
+	unsigned int j;
+	char *p;
 
-        len1 = 0;
-        i = 0;
-        j = 0;
+	l = 0;
+	i = 0;
+	j = 0;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[l])
+		l += 1;
+	p = malloc(l + n + 1);
+	if (p == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i += 1;
+	}
 
-        while (s1[len1])
-                len1 += 1;
-        arr = malloc(len1 + n + 1);
-
-        if (arr == NULL)
-                return (NULL);
-
-        while (s1[i])
-        {
-                arr[i] = s1[i];
-                i += 1;
-        }
-
-        while (j < n)
-        {
-                arr[i + j] = s2[j];
-                j += 1;
-        }
-        arr[i + j] = '\0';
-        return (p);
+	while (j < n)
+	{
+		p[i + j] = s2[j];
+		j += 1;
+	}
+	p[i + j] = '\0';
+	return (p);
 }
