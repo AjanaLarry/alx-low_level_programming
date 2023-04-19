84a83d77729cff583ab40510cb3aa87089278ccf
Juwon Ajana <juwon4real007@gmail.com>
2023-04-18 18:21:40 -0700

    Task 0 - Update dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,6 +9,7 @@
 *@name: name of dog
 *@age: age of dog
 *@owner: owner's name
+*Description: Defines metadata about the dog and type of dogs
 **/
 
 struct dog
@@ -23,6 +24,8 @@ struct dog
 **/
 typedef struct dog dog_t;
 
+/* Additional function prototypes */
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
