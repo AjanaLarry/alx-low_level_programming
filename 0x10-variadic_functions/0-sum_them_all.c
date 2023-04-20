6cc1585f580dc4ce380db78e9d09bdf8eece604b
Juwon Ajana <juwon4real007@gmail.com>
2023-04-20 12:07:50 -0700

    Task 0 - Beauty is variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 /**
 * sum_them_all - function that sums 
@@ -11,17 +12,18 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-       	int sum;
+       	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
 	va_start(args, n);
-	sum = 0;
+	
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
+
 	va_end(args);
 	return (sum);
 }
