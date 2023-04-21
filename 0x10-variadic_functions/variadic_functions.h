30edb1648b9cc7f7e0b258549efcb679c06fe09d
Juwon Ajana <juwon4real007@gmail.com>
2023-04-20 17:36:45 -0700

    Update header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 typedef struct format
 {
@@ -8,11 +12,6 @@ typedef struct format
 	void (*func)(va_list vargs);
 } frm_t;
 
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
 
