61ce6a8b7c9436cd34720716b57d3e9957cff4cf
Juwon Ajana <juwon4real007@gmail.com>
2023-04-27 15:58:37 -0700

    Task 2 - Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* add_node - add a new node to the beginning of a linked_list
+* @head: points to a linked_list
+* @str: string
+* Return: linked_list
+**/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *string;
+	list_t *new;
+	int i;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	copy_str = strdup(str);
+	for (i = 0; str[i] != '\0';)
+		i += 1;
+
+	ret_list->str = string;
+	ret_list->next = *head;
+	ret_list->len = i;
+	*head = new;
+	return (*head);
+}
