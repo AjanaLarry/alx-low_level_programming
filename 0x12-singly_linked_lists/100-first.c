f0bfa3e6e5794fb31723b55a11f729064a3f066f
Juwon Ajana <juwon4real007@gmail.com>
2023-04-27 16:42:29 -0700

    Task 100 - The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void primary(void) __attribute__((constructor));
+
+/**
+* primary - a func that prints a message
+* Return: void
+*/
+
+void primary(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
