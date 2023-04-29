60c0d8f505efe92c00bf186bca8720eaf9abc3d9
Juwon Ajana <juwon4real007@gmail.com>
2023-04-29 14:49:54 -0700

    Task 4 - Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_list - frees a linked_list
+* @head: linked_list head
+* Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *free_pt;
+
+	while (head != NULL)
+	{
+		free_pt = head;
+		head = head->next;
+		free(free_pt);
+	}
+}
