8df24494e541a845ec7ace41f2f135e09264c63e
Juwon Ajana <juwon4real007@gmail.com>
2023-05-02 17:16:40 -0700

    Task 2 - Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,13 +10,13 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_n;
-	if (head == NULL)
-		return (NULL);
+
 	new_n = malloc(sizeof(listint_t));
-	if (new_n == NULL)
+	if (!new_n)
 		return (NULL);
 	new_n->n = n;
 	new_n->next = *head;
 	*head = new_n;
+
 	return (new_n);
 }
