f645c82a8f4cd507c87ca4c0d11ebb30df75139a
Juwon Ajana <juwon4real007@gmail.com>
2023-05-04 16:00:04 -0700

    Task 4 - 100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - func that sets the value of a 
+ * bit to 0 at a given index
+ * @n: integer
+ * @index: index to set 1
+ * Return: int value (1 success, -1 error)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(*n) * 8))
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
